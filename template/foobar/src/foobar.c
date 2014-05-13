ec9b89026f7eacdb00c5067b4294155f7c66292b
Doro Wu <fcwu.tw@gmail.com>
2014-05-13 15:40:33 +0800

    Initialize foobar package

---
+++ template/foobar/src/foobar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void foo()
+{
+    printf("Hello, I'm a shared library\n");
+}
---
+++ template/foobar/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void foo();
+ 
+int main()
+{
+    printf("Hello, main\n");
+    foo();
+    return 0;
+}
