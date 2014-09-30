eec1c4d1e69a19ef81a7cea2fb12e697f629a8fc
Walker Lee <walkerlee.tw@gmail.com>
2014-09-30 10:00:55 +0000

    Add bash/fish/zsh completion template

---
+++ completions/fish/qdk2.fish
@@ -0,0 +1,49 @@
+#
+# Completions for the qdk2 command
+#
+
+#
+# Functions
+#
+
+function __fish_qdk2_using_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -gt 1 ]
+    if [ $argv[1] = $cmd[2] ]
+      return 0
+    end
+  end
+  return 1
+end
+
+# All qdk2 commands
+#complete -c qdk2 -n '__fish_use_subcommand' -xa import --description "Create a new QPKG from the contents of a tarball or URI"
+complete -c qdk2 -n '__fish_use_subcommand' -xa create --description "Create a new QPKG from the template"
+complete -c qdk2 -n '__fish_use_subcommand' -xa build --description "Build a QPKG from a folder"
+complete -c qdk2 -n '__fish_use_subcommand' -xa changelog --description "Tool for maintenance of the QNAP/changelog file in a source package"
+complete -c qdk2 -n '__fish_use_subcommand' -xa extract --description "Extract QNAP App(*.qpkg) or Firmware image(*.img)"
+complete -c qdk2 -n '__fish_use_subcommand' -xa version --description "Show the QDK2 version information"
+
+#
+# qdk2 import
+#
+
+#
+# qdk2 create
+#
+
+#
+# qdk2 build
+#
+
+#
+# qdk2 changelog
+#
+
+#
+# qdk2 extract
+#
+
+#
+# qdk2 version
+#
--- python/settings.py
+++ python/settings.py
@@ -16,7 +16,7 @@ else:
     QDK_BINARY = 'QDK_2.2_amd64'
 
 
-VERSION = 'v0.8-3-gd7f6d21'
+VERSION = 'v0.8-4-g537de08'
 
 
 class Settings(object):
