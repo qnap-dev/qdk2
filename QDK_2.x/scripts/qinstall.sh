179220b53e0dd36e640d5ccde4c2c0a42f2eb0af
Walker Lee <walkerlee.tw@gmail.com>
2018-11-21 16:47:14 +0800

    restore: backward compatibility (QPKG_DISPLAY_NAME and QPKG_DISPLAYNAME)

--- QDK_2.x/scripts/qinstall.sh
+++ QDK_2.x/scripts/qinstall.sh
@@ -143,6 +143,9 @@ CMD_PKG_TOOL=
 . qpkg.cfg
 
 # Backward compatibility
+if [ -z "$QPKG_DISPLAY_NAME" ]; then
+	QPKG_DISPLAY_NAME=$QPKG_DISPLAYNAME
+fi
 
 ###########################################
 # System messages
