ce785e45d129268e69a9b497b671f71c5333047d
ED Hong <edhongcy@gmail.com>
2019-02-23 14:08:10 +0800

    Add qnap internal code signing and anti-tampering support

---
+++ QDK_2.x/scripts/codesigning_login.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SERVER=172.17.21.68
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+read -p 'username: ' USERNAME
+read -sp 'password: ' PASSWORD
+echo
+RESPONSE="$(eval "curl -X POST --cacert ${SCRIPTPATH}/codesigning_cert.pem -d \"username=${USERNAME}&password=${PASSWORD}\" \
+	https://${SERVER}:5000/login 2>/dev/null")"
+RET=$?
+# If curl not installed
+if [ $RET -eq 127 ]; then
+	echo 'Cannot find curl command'
+	exit 1
+fi
+if [ $RET -eq 7 ]; then
+	echo 'Failed to connect to server'
+	exit 1
+fi
+if ! [ $RET -eq 0 ]; then
+	echo 'Error when running curl command'
+	exit 1
+fi
+ERROR="$(echo "$RESPONSE" | python -c 'import sys, json; print json.load(sys.stdin)["error"]')"
+MSG="$(echo "$RESPONSE" | python -c 'import sys, json; print json.load(sys.stdin)["msg"]')"
+if [ "$ERROR" -ne 0 ]; then
+	echo $MSG
+	exit 1
+fi
+TOKEN="$(echo "$RESPONSE" | python -c 'import sys, json; print json.load(sys.stdin)["token"]')"
+echo "Token: $TOKEN"
---
+++ QDK_2.x/scripts/codesigning_qpkg_cms.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import logging
+import subprocess
+import shutil
+import codesigning_common
+
+def check_qpkg_conf(kwargs):
+    # Check if qpkg.cfg exists, and read paramaters from it
+
+    qpkg_conf = os.path.join(kwargs["cwd"],"qpkg.cfg")
+    if not os.path.isfile(qpkg_conf):
+        logging.error("Cannot find qpkg.cfg")
+        sys.exit(1)
+    commands = ["source %s" % qpkg_conf, "echo $QPKG_NAME"]
+    sp = subprocess.Popen("bash",stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+    for cmd in commands:
+        sp.stdin.write(cmd + "\n")
+    sp.stdin.close()
+    kwargs["qpkgname"] = sp.stdout.read().strip()
+
+    commands = ["source %s" % qpkg_conf, "echo $QPKG_VER"]
+    sp = subprocess.Popen("bash",stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+    for cmd in commands:
+        sp.stdin.write(cmd + "\n")
+    sp.stdin.close()
+    kwargs["version"] = sp.stdout.read().strip()
+
+    if kwargs["qpkgname"] == "":
+        logging.error("Cannot find QPKG_NAME in qpkg.cfg")
+        sys.exit(1)
+    if kwargs["version"] == "":
+        logging.error("Cannot find QPKG_VER in qpkg.cfg")
+        sys.exit(1)
+
+def sign_and_save_cms(kwargs):
+    # Send to server
+    kwargs["file"] = kwargs["in"]
+    response = codesigning_common.sign_cms(kwargs)
+    if response["error"] != 0:
+        logging.error("Error message from server when signing cms: %s" % response["msg"])
+        sys.exit(1)
+    signature = response["signature"]
+
+    # Save to file
+    signature_file_name = kwargs["out"]
+    signature_file = open(signature_file_name, "w")
+    signature_file.write(signature)
+    signature_file.close()
+
+def print_usage():
+    usage_string = """
+    Usage:
+      Sign a single file using openssl cms:
+        python codesigning_qpkg_cms.py server=x.x.x.x:port cwd=working_directory cert=certificate.pem
+          in=input_file out=output_file
+
+      working directory: the folder where developers run build.sh
+      cert: certificate of code signing server (optional)
+
+      Sign without cfg:
+        python codesigning_qpkg_cms.py server=x.x.x.x:port qpkgname=qpkg_name version=qpkg_version cert=certificate.pem
+          in=input_file out=output_file
+    """
+    print (usage_string)
+
+if __name__ == "__main__":
+    # Config logging
+    codesigning_common.config_logging()
+
+    try:
+        kwargs = dict(arg.split('=') for arg in sys.argv[1:])
+    except:
+        print_usage()
+        sys.exit(1)
+    codesigning_common.check_args(kwargs)
+    kwargs["token"] = codesigning_common.get_token()
+    kwargs["key_type"] = "qpkg"
+    if "cwd" in kwargs:
+        codesigning_common.check_cwd(kwargs)
+        check_qpkg_conf(kwargs)
+    else:
+        if kwargs["qpkgname"] == "":
+            logging.error("qpkgname not provided")
+            sys.exit(1)
+        if kwargs["version"] == "":
+            logging.error("version not provided")
+            sys.exit(1)
+    sign_and_save_cms(kwargs)
